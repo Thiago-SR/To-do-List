e00a1bc314b8e2e40851eef0291832d8975657aa
Thiago Rodrigues <95245787+Thiago-SR@users.noreply.github.com>
2025-05-30 23:09:26 -0300

    Criação do component Todo

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,44 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-
+import Todo from './components/Todo'
+import TodoForm from './components/TodoForm'
 function App() {
-  const [count, setCount] = useState(0)
+  const [todos, setTodos] = useState([{
+    id: 1,
+    text: 'Estudar React',
+    category: 'Faculdade',
+    completed: false,
+  },
+  {
+    id: 2,
+    text: 'Estudar Node',
+    category: 'Faculdade',
+    completed: false,
+  },
+  {
+    id: 3,
+    text: 'Fazer exercícios',
+    category: 'Pessoal',
+    completed: false,
+  },
+  {
+    id: 4,
+    text: 'Concluir projeto',
+    category: 'Trabalho',
+    completed: false,
+  },
+])
+ 
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  return <div className="app">
+    <h1>Lista de Tarefas</h1>
+    <div className="todo-list">
+      {todos.map((todo)=>(
+        <Todo  todo={todo}/>
+      ))}
+    </div>
+    <TodoForm/>
+  </div>
 }
 
 export default App
