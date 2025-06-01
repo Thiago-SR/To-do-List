63fa6ba2199dd2455dd0a214c665c94f03c47dea
Thiago Rodrigues <95245787+Thiago-SR@users.noreply.github.com>
2025-05-31 22:04:56 -0300

    criação de funções de adicionar tarefa, remover e alterar status para completa

--- src/App.jsx
+++ src/App.jsx
@@ -27,17 +27,38 @@ function App() {
     category: 'Trabalho',
     completed: false,
   },
-])
- 
+]);
 
+const addTodo = (text, category) => {
+  const newTodo = [...todos, {
+    id: Math.random() * 1000,
+    text,
+    category,
+    completed: false,
+  }];
+  setTodos(newTodo);
+};
+
+const removeTodo = (id) => {
+  const newTodo = todos.filter((todo) => todo.id !== id);
+  setTodos(newTodo);
+};
+
+const completeTodo = (id) => {
+  const newTodo = todos.map((todo) => {
+    if(todo.id === id) todo.completed = !todo.completed;
+    return todo;
+  });
+  setTodos(newTodo);
+}
   return <div className="app">
     <h1>Lista de Tarefas</h1>
     <div className="todo-list">
       {todos.map((todo)=>(
-        <Todo  todo={todo}/>
+        <Todo key={todo.id} todo={todo} removeTodo={removeTodo} completeTodo={completeTodo}/>
       ))}
     </div>
-    <TodoForm/>
+    <TodoForm addTodo={addTodo}/>
   </div>
 }
 
